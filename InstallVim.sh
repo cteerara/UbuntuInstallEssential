00db0112f27281c27787dfa1a90511291f553f41
cteerara <cteerara@cteerara.desktop>
2019-10-16 19:14:32 -0600

    Fixed Pathogen path. Add curl download

--- InstallVim.sh
+++ InstallVim.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-
+cwd=$(readlink -f .)
+echo $cwd
+haii
 #-- Install the text editor Vim and a couple useful vim plugins. For more plugin installation, see pathogen plugin manager 
 apt-get -y install vim
 # Move header docs
 cp ./templates/*_template.txt ~/Documents
-# Move plugins and pathogen
-cp -r ./autoload ~/.vim/
+# Install pathogen
+apt install -y curl
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cp pathogen.vim ~/.vim/autoload
+# Move plug.vim to autoload
+cp -r ./autoload/plug.vim ~/.vim/autoload
 cd ~/.vim/
-mkdir -p bundle
 cd bundle
 # Install vim plugins (Require internet access)
 git clone git://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
@@ -15,7 +21,9 @@ git clone git://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
 git clone https://github.com/suan/vim-instant-markdown.git
 # Install dependencies for instant-markdown
-apt install npm
+apt install -y npm
 npm -g install instant-markdown-d
+cd ../..
 # Move vimrc
+cd $cwd
 cp ./.vimrc ~/ 
